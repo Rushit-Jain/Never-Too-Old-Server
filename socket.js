648d25238bf01f920cc1aa35f9439e9c99e70d6c
Rushit Jain <rushit07@gmail.com>
2021-12-13 22:17:10 +0530

    tp commit

---
+++ socket.js
@@ -0,0 +1,17 @@
+let io;
+
+module.exports = {
+  initServer: (httpServer) => {
+    io = require("socket.io")(httpServer, {
+      cors: {
+        origin: "http://192.168.0.112",
+        methods: ["GET", "POST"],
+      },
+    });
+    return io;
+  },
+  getIO: () => {
+    if (!io) throw new Error("Socket io not initialized");
+    return io;
+  },
+};
