b65e6193b3b3c1318c720c85934611087b38bcaf
Rushit Jain <rushit07@gmail.com>
2021-12-13 22:57:29 +0530

    socket done

--- socket.js
+++ socket.js
@@ -4,15 +4,7 @@ module.exports = {
   initServer: (httpServer) => {
     io = require("socket.io")(httpServer, {
       cors: {
-<<<<<<< HEAD
-<<<<<<< HEAD
         origin: "http://192.168.0.112",
-=======
-        origin: "http://192.168.0.104",
->>>>>>> 22d0abbd56a78b9bcff66e42c5723b7cfd8c4432
-=======
-        origin: "http://192.168.0.112",
->>>>>>> a754986e56422666a21514014042778299d6e911
         methods: ["GET", "POST"],
       },
     });
