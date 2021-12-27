3b10745e3e7dcc5525fd0a407ca2f97ed628a62d
Rushit Jain <rushit07@gmail.com>
2021-12-28 00:25:34 +0530

    minor changes after call log ui

--- controller/call-controller.js
+++ controller/call-controller.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const HttpError = require('../models/http-error');
-const Call = require('../models/call-model');
+const HttpError = require("../models/http-error");
+const Call = require("../models/call-model");
 
 exports.saveCall = async (req, res, next) => {
   let calldata = req.body;
@@ -12,14 +12,17 @@ exports.saveCall = async (req, res, next) => {
       isReceived: calldata.isReceived,
       callType: calldata.callType,
       senderChatID: calldata.senderChatID,
-      receiverChatID: calldata.receiverChatID
+      receiverChatID: calldata.receiverChatID,
     });
-    call.save().then(response => res.json(call)).catch(err => console.log(err));
+    call
+      .save()
+      .then((response) => res.json(call))
+      .catch((err) => console.log(err));
     console.log(call);
   } catch (err) {
     return;
   }
-}
+};
 exports.updateCall = async (req, res, next) => {
   let calldata = req.body;
 
@@ -28,34 +31,43 @@ exports.updateCall = async (req, res, next) => {
     // console.log(call[0].timestamp);
     console.log(new Date(call.timestamp));
     console.log(new Date(calldata.timestamp));
-    let duration = Date.parse(calldata.timestamp) - Date.parse(call[0].timestamp);
+    let duration =
+      Date.parse(calldata.timestamp) - Date.parse(call[0].timestamp);
     // console.log(duration);
-    call = await Call.findOneAndUpdate({ roomID: calldata.roomID }, { isReceived: true, duration: duration });
+    call = await Call.findOneAndUpdate(
+      { roomID: calldata.roomID },
+      { isReceived: true, duration: duration }
+    );
     console.log(call);
   } catch (err) {
     return;
   }
-}
+};
 
 exports.getCalls = async (req, res, next) => {
   try {
     var calls;
     console.log(req.query.senderChatID);
     const senderChatID = req.query.senderChatID;
-    const calls1 = await Call.find({
-      senderChatID: senderChatID,
-    }, { _id: 0, roomID: 0 });
-    const calls2 = await Call.find({
-      receiverChatID: senderChatID,
-    }, { _id: 0, roomID: 0 });
+    const calls1 = await Call.find(
+      {
+        senderChatID: senderChatID,
+      },
+      { _id: 0, roomID: 0 }
+    );
+    const calls2 = await Call.find(
+      {
+        receiverChatID: senderChatID,
+      },
+      { _id: 0, roomID: 0 }
+    );
     calls = [...calls1, ...calls2];
 
-
-    calls.sort((a, b) => Date.parse(a.timestamp) - Date.parse(b.timestamp));
+    calls.sort((a, b) => Date.parse(b.timestamp) - Date.parse(a.timestamp));
     console.log(calls);
     res.json(calls);
   } catch (e) {
     const err = new HttpError(e.message, 500);
     return next(err);
   }
-}
+};
