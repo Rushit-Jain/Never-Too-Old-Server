be1787ff298d82f8fa0e04a61be511e84ede6db8
Rushit Jain <rushit07@gmail.com>
2022-01-15 01:42:16 +0530

    dynamic emergency contact info done, volunteer suggestion done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,17 @@
-const express = require('express');
+const express = require("express");
 // const { check } = require('express-validator');
 
-const usersController = require('../controller/users-controllers');
+const usersController = require("../controller/users-controllers");
 
 const router = express.Router();
 // router.get('/', usersController.getUsers);
 // router.post('/gu', usersController.getUser);
-router.post(
-  '/check',
-  usersController.checkUser
-);
+router.post("/check", usersController.checkUser);
 
-router.post('/save', usersController.saveUser);
-router.post('/updateLocation', usersController.updateLocation);
-router.post('/addNewFriend', usersController.addNewFriend);
-router.post('/insertNewGroup', usersController.insertNewGroup);
+router.post("/save", usersController.saveUser);
+router.post("/updateLocation", usersController.updateLocation);
+router.post("/addNewFriend", usersController.addNewFriend);
+router.post("/addNewVolunteer", usersController.addNewVolunteer);
+router.post("/insertNewGroup", usersController.insertNewGroup);
 
 module.exports = router;
