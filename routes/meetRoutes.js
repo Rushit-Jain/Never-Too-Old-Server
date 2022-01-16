26c580cb1f78c152b83992a2ffce93ceed09bc54
Rushit Jain <rushit07@gmail.com>
2022-01-17 02:58:59 +0530

    momentjs installed, upcoming meets done

--- routes/meetRoutes.js
+++ routes/meetRoutes.js
@@ -8,6 +8,7 @@ router.post("/requestMeet", meetController.saveRequestMeet);
 router.post("/bookMeet", meetController.bookMeet);
 router.get("/getVolunteerMeets", meetController.getVolunteerMeets);
 router.get("/getLatestMeets", meetController.getLatestMeets);
+router.get("/upcomingMeets", meetController.getUpcomingMeets);
 router.put("/acceptMeet", meetController.acceptMeet);
 router.get("/", meetController.getMeet);
 
