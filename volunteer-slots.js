4b4d29355df30f8da720a58e57a04f5f04644ef2
Rushit Jain <rushit07@gmail.com>
2022-03-10 02:03:02 +0530

    volunteers slots fetch from mongodb on crash

--- volunteer-slots.js
+++ volunteer-slots.js
@@ -1,11 +1,17 @@
 let volunteerSlots = {};
+const mongoose = require("mongoose");
+const Volunteer = require("./models/volunteer-model");
 
 module.exports = {
   updateVolunteerSlots: (id, slots) => {
     volunteerSlots[id] = slots;
-    console.log(volunteerSlots);
   },
   getVolunteerSlots: (id) => volunteerSlots[id],
   isSlotPresent: (id, timeOfDay, slot) =>
     volunteerSlots[id][timeOfDay].indexOf(slot) != -1,
+  initialize: async () => {
+    let volunteers = await Volunteer.find({}, { _id: 1, slots: 1 }).lean();
+    volunteers.forEach((v) => (volunteerSlots[v._id] = v.slots));
+    console.log(volunteerSlots);
+  },
 };
