e0dff923c8d570af8186593477f6100c31cdd910
Rushit Jain <rushit07@gmail.com>
2022-03-12 12:46:19 +0530

    send mail service working

--- controller/miscellaneous-controller.js
+++ controller/miscellaneous-controller.js
@@ -1,6 +1,8 @@
 const multer = require("multer");
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
 const private = require("../private.json");
+const fs = require("fs");
 
 exports.uploadFile = async (req, res, next) => {
   var storage = multer.diskStorage({
@@ -32,27 +34,55 @@ exports.uploadFile = async (req, res, next) => {
 
 exports.sendFile = async (req, res, next) => {
   try {
-    // let transporter = nodemailer.createTransport({
-    //   host: "smtp.google.com",
-    //   port: 587,
-    //   secure: true,
-    //   auth: {
-    //     type: "OAuth2",
-    //     user: "nevertoooldapp@gmail.com",
-    //     serviceClient: private.client_id,
-    //     privateKey: private.private_key,
-    //   },
-    //   tls: {
-    //     rejectUnauthorized: false,
-    //   },
-    // });
-    // transporter.verify(function (error, success) {
+    const OAuth2Client = new google.auth.OAuth2(
+      private.CLIENT_ID,
+      private.CLIENT_SECRET,
+      private.REDIRECT_URI
+    );
+    OAuth2Client.setCredentials({ refresh_token: private.REFRESH_TOKEN });
+    const accessToken = await OAuth2Client.getAccessToken();
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: "nevertoooldapp@gmail.com",
+        clientId: private.CLIENT_ID,
+        clientSecret: private.CLIENT_SECRET,
+        refreshToken: private.REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
+    });
+    // transport.verify(function (error, success) {
     //   if (error) {
     //     console.log(error);
     //   } else {
     //     console.log("Server is ready to take our messages");
     //   }
     // });
+    console.log(req.file);
+    const mailOptions = {
+      from: "Never Too Old <nevertoooldapp@gmail.com>",
+      to: [
+        "rushit07@gmail.com",
+        "parthjangid8080114742@gmail.com",
+        "bhaveshbellaney@gmail.com",
+      ],
+      text: "Mail from Server",
+      subject:
+        req.body.name +
+        ", a user, is facing a problem. Phone number: " +
+        req.body.phoneNumber,
+      attachments: [
+        {
+          filename: "problem.mp3",
+          content: fs.createReadStream(
+            process.cwd() + "/uploads/" + req.file.filename
+          ),
+        },
+      ],
+    };
+    const result = await transport.sendMail(mailOptions);
+    return result;
   } catch (err) {
     return next(err);
   }
